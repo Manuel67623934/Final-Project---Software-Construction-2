213ade9c121f88d00dc38e21f85602c57ddf1f51
Sonny Kenyi Taniguchi Lock <sonny.taniguchi@unas.edu.pe>
2021-08-24 23:02:03 -0500

    Agregar archivos de proyecto.

---
+++ DatabaseMinimarket/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DatabaseMinimarket
+{
+    public class Class1
+    {
+    }
+}
---
+++ ModelMinimarket/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ModelMinimarket
+{
+    public class Class1
+    {
+    }
+}
