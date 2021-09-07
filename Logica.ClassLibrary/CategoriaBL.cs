9767e9bdfd5f8869adb434924ba0ecee357f978c
Sonny Kenyi Taniguchi Lock <sonny.taniguchi@unas.edu.pe>
2021-09-07 10:53:34 -0500

    7set

--- Logica.ClassLibrary/CategoriaBL.cs
+++ Logica.ClassLibrary/CategoriaBL.cs
@@ -1,16 +1,25 @@
-﻿using System;
+﻿using Dato.ClassLibrary;
+using Entidad.ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Logica.ClassLibrary
 {
    public class CategoriaBL
     {
-        public string getCategoria()
+        public List<CategoriaBE> getCategoria()
         {
+            CategoriaDA data = new CategoriaDA();
+            List<CategoriaBE> lista;
+            lista = data.getCategoria();
+            var FiltrarCategoria = lista.Where(lista.)
+            lista.Where();
 
+            return lista;
         }
     }
 }
