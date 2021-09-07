1982a3e1f6c952abdf50f1ddb14554a0a181706f
Sonny Kenyi Taniguchi Lock <sonny.taniguchi@unas.edu.pe>
2021-09-07 10:34:27 -0500

    7set

--- Dato.ClassLibrary/CategoriaDA.cs
+++ Dato.ClassLibrary/CategoriaDA.cs
@@ -7,7 +7,7 @@ namespace Dato.ClassLibrary
 {
    public class CategoriaDA
     {
-        public CategoriaBE GetCategoria()
+        public List<CategoriaBE> getCategoria()
         {
             List<CategoriaBE> listacategoria = new List<CategoriaBE>() { 
             new CategoriaBE(){Id =1 , Nombre="Licores",Estado='1',UrlSeo="licores"},
@@ -15,8 +15,8 @@ namespace Dato.ClassLibrary
             new CategoriaBE(){Id =3 , Nombre="Cerveza",Estado='0',UrlSeo="cervezs"},
             new CategoriaBE(){Id =4 , Nombre="Snacks",Estado='1',UrlSeo="snacks"},
             };
-           
-            
+
+            return listacategoria;
         }
     }
 }
