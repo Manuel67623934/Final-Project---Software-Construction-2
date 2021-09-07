11175dc7c353b7f2d6cd14f2eea9a4e1e6dcc9cb
JOSE MANUEL BRAVO RENGIFO <jose.bravo@unas.edu.pe>
2021-09-07 11:16:47 -0500

    df

---
+++ Dato.ClassLibrary/UsuarioDA.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidad.ClassLibrary;
+
+namespace Dato.ClassLibrary
+{
+    public class UsuarioDA
+    {
+        public UsuarioBE GetUsuario()
+        {
+            return new UsuarioBE { User = "admi", Password = "1234"};
+        }
+    }
+}
